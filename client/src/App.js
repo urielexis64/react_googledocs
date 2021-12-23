909faf8df2687b2e2e7eb417d24c3127665e3c61
Uriel Aispuro <urielalexis64@gmail.com>
2021-12-22 23:05:54 -0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import TextEditor from "./TextEditor";
 
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+
+import {v4 as uuidV4} from "uuid";
+
 function App() {
-	return <TextEditor />;
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Navigate to={`/documents/${uuidV4()}`} />} />
+				<Route path='/documents/:id' element={<TextEditor />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
